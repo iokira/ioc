225eb3d5b249afabc5885b3f637e400fbcdf0c37
wurzeit <hiccap.master.jedi@gmail.com>
2023-09-18 23:00:01 +0900

    ファイル出力を作成

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{env, error::Error, fs::File, io::Read, process};
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{Read, Write},
+    process,
+};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,28 +22,35 @@ fn main() {
 }
 
 fn run(input: Input) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(input.filename)?;
+    let mut inputfile = File::open(input.inputfilename)?;
+    let mut outputfile = File::create(input.outputfilename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
+    inputfile.read_to_string(&mut contents)?;
 
-    println!("{}", contents);
+    write!(outputfile, "{}", contents)?;
+    outputfile.flush()?;
 
     Ok(())
 }
 
 struct Input {
-    filename: String,
+    inputfilename: String,
+    outputfilename: String,
 }
 
 impl Input {
     fn new(args: &[String]) -> Result<Input, &'static str> {
-        if args.len() < 2 {
+        if args.len() < 3 {
             return Err("not enough arguments");
         }
 
-        let filename = args[1].clone();
+        let inputfilename = args[1].clone();
+        let outputfilename = args[2].clone();
 
-        Ok(Input { filename })
+        Ok(Input {
+            inputfilename: inputfilename,
+            outputfilename: outputfilename,
+        })
     }
 }
