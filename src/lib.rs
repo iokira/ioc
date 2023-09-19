c3bf232b828d06a5db4d7038f865f35c8054173c
wurzeit <hiccap.master.jedi@gmail.com>
2023-09-19 22:43:02 +0900

    変数名を命名規則に則ったものに変更

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ use std::{
 };
 
 pub struct Input {
-    inputfilename: String,
-    outputfilename: String,
+    input_file_name: String,
+    output_file_name: String,
 }
 
 impl Input {
@@ -15,29 +15,29 @@ impl Input {
             return Err("not enough arguments");
         }
 
-        let inputfilename = args[1].clone();
-        let outputfilename = args[2].clone();
+        let input_file_name = args[1].clone();
+        let output_file_name = args[2].clone();
 
         Ok(Input {
-            inputfilename,
-            outputfilename,
+            input_file_name,
+            output_file_name,
         })
     }
 }
 
 pub fn run(input: Input) -> Result<(), Box<dyn Error>> {
-    let mut inputfile = File::open(input.inputfilename)?;
-    let mut outputfile = File::create(input.outputfilename)?;
+    let mut input_file = File::open(input.input_file_name)?;
+    let mut output_file = File::create(input.output_file_name)?;
 
     let mut contents = String::new();
-    inputfile.read_to_string(&mut contents)?;
+    input_file.read_to_string(&mut contents)?;
 
-    writeln!(outputfile, ".intel_syntax noprefix")?;
-    writeln!(outputfile, ".globl main")?;
-    writeln!(outputfile, "main:")?;
-    writeln!(outputfile, "\tmov rax, {}", contents)?;
-    writeln!(outputfile, "\tret")?;
-    outputfile.flush()?;
+    writeln!(output_file, ".intel_syntax noprefix")?;
+    writeln!(output_file, ".globl main")?;
+    writeln!(output_file, "main:")?;
+    writeln!(output_file, "\tmov rax, {}", contents)?;
+    writeln!(output_file, "\tret")?;
+    output_file.flush()?;
 
     Ok(())
 }
