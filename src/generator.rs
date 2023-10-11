10ab7ccbb5e2a4a14b7e134182ff6fb10c70a6a4
iokira <iam.iokiller@gmail.com>
2023-10-11 15:44:21 +0900

    remove unused import

--- src/generator.rs
+++ src/generator.rs
@@ -1,5 +1,5 @@
 pub mod generator {
-    use crate::{numtype::numtype::NumType, tree::tree::*};
+    use crate::tree::tree::*;
 
     fn generate_val(assembly: &mut String, offset: usize) {
         let str = format!("\tmov rax, rbp\n\tsub rax, {}\n\tpush rax\n", offset);
